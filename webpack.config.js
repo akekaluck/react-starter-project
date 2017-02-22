8e952347cd8ea66478c41bbb6c5cdf1e00ebe1a9
Akekaluck Wesnarat <akekaluckwesnarat@u0145690s-mbp.ten.thomsonreuters.com>
2017-02-22 16:09:06 +0700

    Include less

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var path = require("path");
 
 module.exports = {
   entry: './app/js/app.js',
   output: {
-    path: path.resolve(__dirname, "build/js"),
+    path: path.resolve(__dirname, "build"),
     publicPath: '/build',
-    filename: 'bundle.js'
+    filename: '/js/bundle.js'
   },
+  devtool: 'source-map',
   module: {
     loaders: [
       {
@@ -16,7 +18,19 @@ module.exports = {
         query: {
           presets: ['es2015','react']
         }
+      },
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          //resolve-url-loader may be chained before sass-loader if necessary
+          use: ['css-loader', 'less-loader']
+        })
       }
     ]
-  }
+  },
+  plugins: [
+    // extract inline css into separate 'styles.css'
+    new ExtractTextPlugin('./css/styles.css')
+  ]
 }
